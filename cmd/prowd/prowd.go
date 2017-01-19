56a4c14de03030f18a9d0a2f79697a2759c20555
Matthew Fisher <mfisher@deis.com>
2017-01-19 00:18:58 -0700

    create prowd binary

---
+++ cmd/prowd/prowd.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"io"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	// flagDebug is a signal that the user wants additional output.
+	flagDebug bool
+)
+
+var globalUsage = "The prow server."
+
+func newRootCmd(out io.Writer) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:          "prowd",
+		Short:        "The prow server.",
+		Long:         globalUsage,
+		SilenceUsage: true,
+	}
+	p := cmd.PersistentFlags()
+	p.BoolVar(&flagDebug, "debug", false, "enable verbose output")
+
+	cmd.AddCommand(
+		newStartCmd(out),
+		newVersionCmd(out),
+	)
+
+	return cmd
+}
+
+func main() {
+	cmd := newRootCmd(os.Stdout)
+	if err := cmd.Execute(); err != nil {
+		os.Exit(1)
+	}
+}
