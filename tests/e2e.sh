a7fd02f5f947ab02ffed96cccd7932c4ade79fd0
Matthew Fisher <mfisher@deis.com>
2017-01-25 15:04:51 -0800

    add e2e tests
    closes #2

---
+++ tests/e2e.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+# Usage: ./e2e.sh
+#
+# This script assumes an existing single node k8s cluster with helm and prowd installed. This script assumes
+# that prowd is available at http://k8s.cluster:44135
+# To do this, make a new rule in /etc/hosts
+#
+# $ cat /etc/hosts | grep k8s.cluster
+# 10.245.1.3      k8s.cluster
+#
+# Port 44135 is the port in which prowd is exposed on the host. This will eventually be fixed with
+# https://github.com/deis/prow/issues/1
+
+# fail fast
+set -eof pipefail
+
+CONTAINER_TIMEOUT=10s
+
+cd testdata/example-dockerfile-http
+
+# TODO(bacongobbler): replace this with `prow up` once it is readily available
+# deploy the app
+tar czf build.tar.gz Dockerfile rootfs/
+pushd charts
+tar czf ../chart.tar.gz example-dockerfile-http/
+popd
+curl -XPOST -F release-tar=@build.tar.gz -F chart-tar=@chart.tar.gz http://k8s.cluster:44135/apps/example-dockerfile-http
+
+# wait for the container to come up
+sleep $CONTAINER_TIMEOUT
+
+revision=$(helm list | grep example-dockerfile-http | awk '{print $2}')
+if [[ "$revision" != "1" ]]; then
+	echo "Expected REVISION == 1, got '$revision'"
+	exit 1
+fi
+
+# example-dockerfile-http exposes itself on port 44144
+app_output=$(curl -sS http://k8s.cluster:44144)
+if [[ "$app_output" != "Powered by Prow" ]]; then
+	echo "Expected 'Powered by Prow', got '$app_output'"
+	exit 1
+fi
+
+# TODO(bacongobbler): replace this with `prow up` once it is readily available
+# deploy the app again, changing POWERED_BY and check that the update is seen upstream
+echo "ENV POWERED_BY Kubernetes" >> Dockerfile
+tar czf build.tar.gz Dockerfile rootfs/
+curl -XPOST -F release-tar=@build.tar.gz -F chart-tar=@chart.tar.gz http://k8s.cluster:44135/apps/example-dockerfile-http
+
+sleep $CONTAINER_TIMEOUT
+app_output=$(curl -sS http://k8s.cluster:44144)
+if [[ "$app_output" != "Powered by Kubernetes" ]]; then
+	echo "Expected 'Powered by Kubernetes', got '$app_output'"
+	exit 1
+fi
