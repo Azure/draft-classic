094093e92355e58cf31671b99a1dbe1574cddee6
Matthew Fisher <mfisher@deis.com>
2017-01-30 15:54:05 -0800

    basic scaffolding for `glide up`
    this handles the HTTP connection right up to uploading the current directory.
    Now we need to handle returning http.StatusSwitchingProtocols from the server
    and upgrading the client to a websocket connection.

--- pkg/prow/client.go
+++ pkg/prow/client.go
@@ -1,14 +1,34 @@
 package prow
 
 import (
+	"bytes"
 	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"mime/multipart"
 	"net/http"
 	"net/url"
+	"os"
+	"path"
 
+	log "github.com/Sirupsen/logrus"
+	"github.com/docker/docker/pkg/archive"
 	"k8s.io/helm/pkg/proto/hapi/release"
 
 	"github.com/deis/prow/pkg/version"
 )
+
+// closingBuffer is a bytes.Buffer that implements .Close() so we can use it as an io.ReadCloser.
+type closingBuffer struct {
+	*bytes.Buffer
+}
+
+func (cb *closingBuffer) Close() error {
+	// we don't actually have to do anything here, since the buffer is just some data in memory
+	// and the error is initialized to no-error.
+	return nil
+}
 
 // Client manages client side of the prow-prowd protocol. It wraps an *http.Client with a url
 // Endpoint and common headers to send on every request.
@@ -46,7 +66,73 @@ func NewFromString(endpoint string, client *http.Client) (*Client, error) {
 // Up uploads the contents of appDir to prowd, installs it in the specified namespace and
 // returns a Helm Release.
 func (c Client) Up(appDir, namespace string) (*release.Release, error) {
-	return &release.Release{Info: &release.Info{Status: &release.Status{Code: release.Status_DEPLOYED}}}, nil
+	// this is the multipart form buffer
+	b := closingBuffer{new(bytes.Buffer)}
+
+	appName := path.Base(appDir)
+	log.Debugf("APP NAME: %s", appName)
+
+	log.Debug("assembling build context archive")
+	buildContext, err := tarBuildContext(appDir)
+	if err != nil {
+		return nil, err
+	}
+
+	log.Debug("assembling chart archive")
+	chartTar, err := tarChart(path.Join(appDir, "chart"))
+	if err != nil {
+		return nil, err
+	}
+
+	// Prepare a form to upload the build context and chart archives.
+	w := multipart.NewWriter(&b)
+	buildContextFormFile, err := w.CreateFormFile("release-tar", "build.tar.gz")
+	if err != nil {
+		return nil, err
+	}
+	if _, err = io.Copy(buildContextFormFile, buildContext); err != nil {
+		return nil, err
+	}
+
+	// Add the other fields
+	chartFormFile, err := w.CreateFormFile("chart-tar", "chart.tar.gz")
+	if err != nil {
+		return nil, err
+	}
+	if _, err = io.Copy(chartFormFile, chartTar); err != nil {
+		return nil, err
+	}
+	if err := w.Close(); err != nil {
+		return nil, err
+	}
+
+	c.Endpoint.Path = "/apps/" + appName
+	req := &http.Request{
+		Method:     "POST",
+		URL:        c.Endpoint,
+		Proto:      "HTTP/1.1",
+		ProtoMajor: 1,
+		ProtoMinor: 1,
+		Header:     c.Header,
+		Body:       &b,
+		Host:       c.Endpoint.Host,
+	}
+	req.Header.Set("Content-Type", w.FormDataContentType())
+	resp, err := c.HTTPClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	// NOTE(bacongobbler): after accepting the tarballs, the server upgrades the connection to
+	// a websocket connection, so we need to switch protocols.
+	if resp.StatusCode != http.StatusSwitchingProtocols {
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return nil, fmt.Errorf("server responded with status code %d and failed to read the body: %v", resp.StatusCode, err)
+		}
+		return nil, fmt.Errorf("server responded with status code %d: %s", resp.StatusCode, body)
+	}
+	return nil, nil
 }
 
 // Version returns the server version.
@@ -72,4 +158,51 @@ func (c Client) Version() (*version.Version, error) {
 	dec := json.NewDecoder(resp.Body)
 	err = dec.Decode(&ver)
 	return &ver, err
+}
+
+// exists returns whether the given file or directory exists or not
+func exists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return true, err
+}
+
+// tarBuildContext archives the given directory and returns the archive as an io.ReadCloser.
+func tarBuildContext(path string) (io.ReadCloser, error) {
+	pathExists, err := exists(path)
+	if err != nil {
+		return nil, err
+	}
+
+	if !pathExists {
+		return nil, fmt.Errorf("directory '%s' does not exist", path)
+	}
+
+	options := archive.TarOptions{
+		ExcludePatterns: []string{
+			// do not include the chart directory. That will be packaged separately.
+			"chart",
+		},
+		Compression: archive.Gzip,
+	}
+	return archive.TarWithOptions(path, &options)
+}
+
+// tarChart archives the directory and returns the archive as an io.ReadCloser.
+func tarChart(path string) (io.ReadCloser, error) {
+	pathExists, err := exists(path)
+	if err != nil {
+		return nil, err
+	}
+
+	if !pathExists {
+		return nil, fmt.Errorf("chart directory '%s' does not exist", path)
+	}
+
+	return archive.Tar(path, archive.Gzip)
 }
