4730539ed72e85bf12b2551e441c4f387d963cdf
Matthew Fisher <mfisher@deis.com>
2017-02-09 09:47:15 -0800

    refactor e2e tests

--- tests/e2e.sh
+++ tests/e2e.sh
@@ -1,51 +1,60 @@
 #!/usr/bin/env bash
+#
 # Usage: ./e2e.sh
 #
-# This script assumes an existing single node k8s cluster with helm and prowd installed. This script assumes
-# that prowd is available at http://k8s.cluster:44135
-# To do this, make a new rule in /etc/hosts
-#
-# $ cat /etc/hosts | grep k8s.cluster
-# 10.245.1.3      k8s.cluster
-#
-# Port 44135 is the port in which prowd is exposed on the host. This will eventually be fixed with
-# https://github.com/deis/prow/issues/1
+# This script assumes an existing single node k8s cluster with helm and prowd installed. It
+# installs all the apps under testdata and checks that they pass or fail, depending on their
+# success condition.
 
-# fail fast
-set -eof pipefail
+echo "testing apps that are expected to pass"
+pushd testdata/good > /dev/null
+for app in */; do
+    echo "switching to ${app}"
+    pushd "${app}" > /dev/null
+    # strip trailing forward slash
+    app=${app: : -1}
+    prow up
+    echo "checking that ${app} v1 was released"
+    revision=$(helm list | grep "${app}" | awk '{print $2}')
+    if [[ "$revision" != "1" ]]; then
+        echo "Expected REVISION == 1, got $revision"
+        exit 1
+    fi
+    echo "GOOD"
+    # deploy the app again and check that the update is seen upstream
+    prow up
+    echo "checking that ${app} v2 was released"
+    revision=$(helm list | grep "${app}" | awk '{print $2}')
+    if [[ "$revision" != "2" ]]; then
+        echo "Expected REVISION == 2, got $revision"
+        exit 1
+    fi
+    echo "GOOD"
+    echo "deleting the helm release for ${app}"
+    # clean up
+    helm delete --purge "${app}"
+    echo "GOOD"
+    popd > /dev/null
+done
+popd > /dev/null
 
-CONTAINER_TIMEOUT=${CONTAINER_TIMEOUT:-1m}
+echo "testing apps that are expected to fail"
+pushd testdata/bad > /dev/null
+for app in */; do
+    echo "switching to ${app}"
+    pushd "${app}" > /dev/null
+    # strip trailing forward slash
+    app=${app: : -1}
+    prow up
+    echo "checking that ${app} v1 was NOT released"
+    release=$(helm list | grep "${app}")
+    if [[ "$release" != "" ]]; then
+        echo "Expected no release to exist , got $release"
+        exit 1
+    fi
+    echo "GOOD"
+    popd > /dev/null
+done
+popd > /dev/null
 
-cd testdata/example-dockerfile-http
-
-# TODO(bacongobbler): replace this with `prow up` once it is readily available
-# deploy the app
-prow up
-
-# wait for the container to come up
-sleep $CONTAINER_TIMEOUT
-
-revision=$(helm list | grep example-dockerfile-http | awk '{print $2}')
-if [[ "$revision" != "1" ]]; then
-	echo "Expected REVISION == 1, got '$revision'"
-	exit 1
-fi
-
-# example-dockerfile-http exposes itself on port 44144
-app_output=$(curl -sS http://k8s.cluster:44144)
-if [[ "$app_output" != "Powered by Prow" ]]; then
-	echo "Expected 'Powered by Prow', got '$app_output'"
-	exit 1
-fi
-
-# TODO(bacongobbler): replace this with `prow up` once it is readily available
-# deploy the app again, changing POWERED_BY and check that the update is seen upstream
-echo "ENV POWERED_BY Kubernetes" >> Dockerfile
-prow up
-
-sleep $CONTAINER_TIMEOUT
-app_output_2=$(curl -sS http://k8s.cluster:44144)
-if [[ "$app_output_2" != "Powered by Kubernetes" ]]; then
-	echo "Expected 'Powered by Kubernetes', got '$app_output_2'"
-	exit 1
-fi
+echo "e2e tests finished."
