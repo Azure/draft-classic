95f592cb80583dd59749d373f8b247cbae3295e0
Matthew Fisher <mfisher@deis.com>
2017-03-16 11:22:04 -0700

    beef up load_test.go

--- pkg/prow/pack/load_test.go
+++ pkg/prow/pack/load_test.go
@@ -1,6 +1,10 @@
 package pack
 
 import (
+	"io/ioutil"
+	"os"
+	"path"
+	"path/filepath"
 	"testing"
 )
 
@@ -38,6 +42,66 @@ func TestFromDir(t *testing.T) {
 
 	if string(pack.DetectScript) != expectedDetect {
 		t.Errorf("expected detect == expected, got '%v'", pack.DetectScript)
+	}
+
+	if _, err := FromDir("dir-does-not-exist"); err == nil {
+		t.Errorf("expected err to be non-nil when path does not exist")
+	}
+
+	// post-cleanup: switch back to the cwd so other tests continue to work.
+	cwd, err := os.Getwd()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	dir, err := ioutil.TempDir("", "prow-pack-test")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err := os.Chdir(dir); err != nil {
+		t.Fatal(err)
+	}
+	// remove the dir from under our feet to force filepath.Abs to fail
+	os.RemoveAll(dir)
+	if _, err := FromDir("."); err == nil {
+		t.Errorf("expected err to be non-nil when filepath.Abs(\".\") should fail")
+	}
+
+	if err := os.Chdir(cwd); err != nil {
+		t.Fatal(err)
+	}
+
+	if os.Getenv("CI") != "" {
+		t.Skip("skipping file permission mode tests on CI servers")
+	}
+
+	// re-use the dir
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(dir)
+	if _, err := Create(path.Base(dir), path.Dir(dir)); err != nil {
+		t.Fatal(err)
+	}
+	// load a pack with an un-readable Dockerfile (file perms 0000)
+	if err := os.Chmod(filepath.Join(dir, DockerfileName), 0000); err != nil {
+		t.Fatal(err)
+	}
+	if _, err := FromDir(dir); err == nil {
+		t.Errorf("expected err to be non-nil when reading the Dockerfile")
+	}
+
+	// revert file perms for the Dockerfile in prep for the detect scrit
+	if err := os.Chmod(filepath.Join(dir, DockerfileName), 0644); err != nil {
+		t.Fatal(err)
+	}
+
+	// make the detect script available but unreadable
+	if err := os.Chmod(filepath.Join(dir, DetectName), 0000); err != nil {
+		t.Fatal(err)
+	}
+	if _, err := FromDir(dir); err == nil {
+		t.Errorf("expected err to be non-nil when reading the detect script")
 	}
 }
 
