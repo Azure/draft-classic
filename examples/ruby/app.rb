35d6ac90860a01a8225736e97f69b440e5519d1b
Seth Goings <segoings@microsoft.com>
2017-05-30 22:48:23 -0600

    fix(examples/ruby): use sinatra
    also wire up readiness and liveness probes properly

--- examples/ruby/app.rb
+++ examples/ruby/app.rb
@@ -1,42 +1,8 @@
-require 'socket' # Provides TCPServer and TCPSocket classes
+require 'sinatra'
 
-# Initialize a TCPServer object that will listen
-# on localhost:2345 for incoming connections.
-server = TCPServer.new('localhost', 8080)
+set :bind, '0.0.0.0'
+set :port, 8080
 
-# loop infinitely, processing one incoming
-# connection at a time.
-loop do
-
-  # Wait until a client connects, then return a TCPSocket
-  # that can be used in a similar fashion to other Ruby
-  # I/O objects. (In fact, TCPSocket is a subclass of IO.)
-  socket = server.accept
-
-  # Read the first line of the request (the Request-Line)
-  request = socket.gets
-
-  # Log the request to the console for debugging
-  STDERR.puts request
-
-  response = "Hello World, I'm Ruby!\n"
-
-  # We need to include the Content-Type and Content-Length headers
-  # to let the client know the size and type of data
-  # contained in the response. Note that HTTP is whitespace
-  # sensitive, and expects each header line to end with CRLF (i.e. "\r\n")
-  socket.print "HTTP/1.1 200 OK\r\n" +
-               "Content-Type: text/plain\r\n" +
-               "Content-Length: #{response.bytesize}\r\n" +
-               "Connection: close\r\n"
-
-  # Print a blank line to separate the header from the response body,
-  # as required by the protocol.
-  socket.print "\r\n"
-
-  # Print the actual response body, which is just "Hello World!\n"
-  socket.print response
-
-  # Close the socket, terminating the connection
-  socket.close
+get '/' do
+  'Hello World, I\'m Ruby!'
 end
