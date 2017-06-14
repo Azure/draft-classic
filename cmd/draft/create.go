a048e4640a2f32bf19f62954c163de6d5a56121d
Didier Roche <didrocks@ubuntu.com>
2017-06-14 08:56:42 +0200

    Add \n to the scaffold draftignore example.

--- cmd/draft/create.go
+++ cmd/draft/create.go
@@ -117,7 +117,7 @@ func (c *createCmd) run() error {
 
 	ignoreFile := filepath.Join(c.dest, ignoreFileName)
 	if _, err := os.Stat(ignoreFile); os.IsNotExist(err) {
-		d1 := []byte("*.swp\n*.tmp\n*.temp\n.git*")
+		d1 := []byte("*.swp\n*.tmp\n*.temp\n.git*\n")
 		if err := ioutil.WriteFile(ignoreFile, d1, 0644); err != nil {
 			return err
 		}
