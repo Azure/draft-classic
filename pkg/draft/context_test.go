7641f12db048677f34fa23188370cedcca5d92c6
Michelle Noorali <michelle@deis.com>
2017-09-12 13:23:49 -0400

    ref(pkg/draft): remove ondraft val

--- pkg/draft/context_test.go
+++ pkg/draft/context_test.go
@@ -15,8 +15,7 @@ image:
   name: ""
   org: ""
   registry: ""
-  tag: da39a3ee5e6b4b0d3255bfef95601890afd80709
-ondraft: true`
+  tag: da39a3ee5e6b4b0d3255bfef95601890afd80709`
 
 func newTestServer() *Server {
 	cfg := &ServerConfig{
