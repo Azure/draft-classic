c36efe426bb9b56387960090f1aba8ea986c2772
Matthew Fisher <matt.fisher@microsoft.com>
2017-09-20 09:22:42 -0700

    port linguist's .gitattributes support to Go
    This provides users with a way to override `draft create`'s language detection logic by manually specifying if the files should be included for detection or if it is a different language type than what is determined by linguist.
    
    See https://github.com/github/linguist#overrides

--- pkg/linguist/linguist_test.go
+++ pkg/linguist/linguist_test.go
@@ -4,9 +4,11 @@ import (
 	"testing"
 )
 
-const appPythonPath = "testdata/app-python"
-
-const appEmptydirPath = "testdata/app-emptydir"
+const (
+	appPythonPath        = "testdata/app-python"
+	appEmptydirPath      = "testdata/app-emptydir"
+	appGitAttributesPath = "testdata/app-gitattributes"
+)
 
 func TestProcessDir(t *testing.T) {
 	output, err := ProcessDir(appPythonPath)
@@ -26,6 +28,15 @@ func TestProcessDir(t *testing.T) {
 	output, _ = ProcessDir(appEmptydirPath)
 	if len(output) != 0 {
 		t.Errorf("expected no languages detected, got '%d'", len(output))
+	}
+
+	// test an app with a .gitattributes file specifying the language type
+	output, err = ProcessDir(appGitAttributesPath)
+	if err != nil {
+		t.Error("expected detect to pass")
+	}
+	if output[0].Language != "Duck" {
+		t.Errorf("expected output == 'Duck', got '%s'", output[0].Language)
 	}
 }
 
