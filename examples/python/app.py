c418382245ab1e46f6399903ac5e3d15d3deea37
Matthew Fisher <matt.fisher@microsoft.com>
2017-10-05 08:40:15 -0700

    Revert "do not log requests from kubernetes liveness/readiness probes"
    This reverts commit ac980f8133655af28e6a6faf0044a043dffd526c.

--- examples/python/app.py
+++ examples/python/app.py
@@ -1,45 +1,9 @@
-import logging
-import logging.config
-import os
-import sys
-import time
-
-from flask import Flask, request
-
-RUNNING_IN_KUBERNETES = 'KUBERNETES_SERVICE_HOST' in os.environ
-
+from flask import Flask
 app = Flask(__name__)
-handler = logging.StreamHandler(sys.stdout)
-handler.setLevel(logging.INFO)
-app.logger.addHandler(handler)
 
-# disable flask's default request logger
-werkzeug_logger = logging.getLogger('werkzeug')
-werkzeug_logger.setLevel(logging.ERROR)
-
-logging.getLogger().setLevel(logging.INFO)
-
-@app.route('/', methods=['GET'])
+@app.route('/')
 def hello_world():
     return "Hello, World!\n"
 
-@app.after_request
-def log_after(response):
-    # check if the request was made from a kubernetes liveness/readiness probe
-    if RUNNING_IN_KUBERNETES:
-        if request.headers.get('User-Agent') == 'Go-http-client/1.1':
-            return response
-    app.logger.info('%s %s %s %s %s %s',
-        time.strftime('[%Y-%b-%d %H:%M]'),
-        request.remote_addr,
-        request.method,
-        request.scheme,
-        request.full_path,
-        response.status)
-    return response
-
 if __name__ == '__main__':
-    host = '0.0.0.0'
-    port = int(os.environ.get('PORT', '8080'))
-    app.logger.info('Serving on http://%s:%d', host, port)
-    app.run(host=host, port=port)
+    app.run(host='0.0.0.0', port=8080)
--- examples/ruby/app.rb
+++ examples/ruby/app.rb
@@ -1,26 +1,8 @@
 require 'sinatra'
-require 'logger'
 
-configure do
-  set :bind, '0.0.0.0'
-  set :port, 8080
-  disable :logging
-  logger = Logger.new STDOUT
-  logger.level = Logger::INFO
-  logger.datetime_format = '%a %d-%m-%Y %H%M '
-  set :logger, logger
-end
+set :bind, '0.0.0.0'
+set :port, 8080
 
 get '/' do
   'Hello World, I\'m Ruby!'
-end
-
-after do
-  # disable logging for kubernetes liveness/readiness probes
-  if ENV.has_key? "KUBERNETES_SERVICE_HOST"
-    if request.user_agent == 'Go-http-client/1.1'
-      return
-    end
-  end
-  settings.logger.info "#{response.status} #{response.body}"
 end
