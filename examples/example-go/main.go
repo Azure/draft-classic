37d4fc8eb3d1eeb47c95020fb9ce907c9613442b
Radu Matei <matei.radu94@gmail.com>
2018-04-20 23:06:46 +0300

    Fix tests and documentation for chart and dockerfile in environment manifest

--- examples/example-go/main.go
+++ examples/example-go/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World, I'm Golang 123!")
+	fmt.Fprintf(w, "Hello World, I'm Golang!")
 }
 
 func main() {
