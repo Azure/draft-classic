3caecf0d0f31135856d9830739535cde9c27e070
Michelle Noorali <michellemolu@gmail.com>
2018-05-08 11:05:25 -0400

    ref(cmd/draft): add test for config flag on init

---
+++ cmd/draft/init_test.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"io/ioutil"
+	"testing"
+
+	"github.com/Azure/draft/pkg/draft/draftpath"
+)
+
+func TestParseConfig(t *testing.T) {
+	testCases := []struct {
+		configFile  string
+		expectErr   bool
+		pluginCount int
+		repoCount   int
+	}{
+		{"", false, 0, 0},
+		{"testdata/init/configFile.toml", false, 1, 1},
+		{"testdata/init/malformedConfigFile.toml", true, 0, 0},
+		{"testdata/init/missingConfigFile.toml", true, 0, 0},
+	}
+
+	for _, tc := range testCases {
+		resetEnvVars := unsetEnvVars()
+		tempHome, teardown := tempDir(t, "draft-init")
+		defer func() {
+			teardown()
+			resetEnvVars()
+		}()
+
+		cmd := &initCmd{
+			home:       draftpath.Home(tempHome),
+			out:        ioutil.Discard,
+			configFile: tc.configFile,
+		}
+
+		plugins, repos, err := cmd.parseConfig()
+		if err != nil && !tc.expectErr {
+			t.Errorf("Not expecting error but got error: %v", err)
+		}
+		if len(plugins) != tc.pluginCount {
+			t.Errorf("Expected %v plugins, got %#v", tc.pluginCount, len(plugins))
+		}
+		if len(repos) != tc.repoCount {
+			t.Errorf("Expected %v pack repos, got %#v", tc.repoCount, len(repos))
+		}
+	}
+}
