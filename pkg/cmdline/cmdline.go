5913eb3b364224f3dc2ad9158e46fb705a3ef480
Simon Jürgensmeyer <Simon.Juergensmeyer@gmail.com>
2018-10-01 17:16:31 +0200

    Added quiet flag

--- pkg/cmdline/cmdline.go
+++ pkg/cmdline/cmdline.go
@@ -125,29 +125,29 @@ func Display(ctx context.Context, app string, summaries <-chan *builder.Summary,
 
 func progress(cli *cmdline, app, desc string, codes <-chan builder.SummaryStatusCode) {
 	start := time.Now()
-	done := make(chan string, 1)
+	done := make(chan builder.SummaryStatusCode, 1)
 	go func() {
 		defer close(done)
 		for code := range codes {
-			switch code {
-			case builder.SummarySuccess:
-				done <- fmt.Sprintf("%s: %s  (%.4fs)\n", cyan(app), passStr(desc), time.Since(start).Seconds())
-				return
-			case builder.SummaryFailure:
-				done <- fmt.Sprintf("%s: %s  (%.4fs)\n", cyan(app), failStr(desc), time.Since(start).Seconds())
-				return
+			if code == builder.SummarySuccess || code == builder.SummaryFailure {
+				done <- code
 			}
 		}
-		done <- "\n"
 	}()
 	m := fmt.Sprintf("%s: %s", cyan(app), yellow(desc))
 	s := `-\|/-`
 	i := 0
 	for {
 		select {
-		case msg := <-done:
-			fmt.Fprintf(cli.opts.stdout, "\r%s", msg)
-			return
+		case code := <-done:
+			switch code {
+			case builder.SummarySuccess:
+				fmt.Fprintf(cli.opts.stdout, "\r%s: %s  (%.4fs)\n", cyan(app), passStr(desc), time.Since(start).Seconds())
+				return
+			case builder.SummaryFailure:
+				fmt.Fprintf(cli.opts.stderr, "\r%s: %s  (%.4fs)\n", cyan(app), failStr(desc), time.Since(start).Seconds())
+				return
+			}
 		default:
 			fmt.Fprintf(cli.opts.stdout, "\r%s %c", m, s[i%len(s)])
 			time.Sleep(50 * time.Millisecond)
